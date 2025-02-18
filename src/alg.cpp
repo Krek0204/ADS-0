a27dfb288c68a7cfb808850f8a2a34d9294378e7
Krek0204 <vlad020406@yandex.ru>
2025-02-18 11:37:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,13 +3,13 @@ int gcd(int a, int b) {
   int x = 0;
   if (a > b) {
     for (int i = 1; i <= a; i++) {
-      if ((a % i == 0) && (b % i == 0))
+      if ((a % i == 0) && (b % i == 0)){
       x = i;
+      }
     }
-  }
-  else {
+  } else {
     for (int i = 1; i <= b; i++) {
-	    if ((a % i == 0) && (b % i == 0))
+      if ((a % i == 0) && (b % i == 0))
 	      x = i;
     }
   }
